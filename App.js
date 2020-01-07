24094a26b63927a23dd78646ccff5c9656cb40de
kasselTrankos <alvaro.touzon@gmail.com>
2020-01-07 21:25:00 +0100

    feat() - add mine components - block duplicate react-native from hasImpl - runnnn

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import {WaterIndicator} from '@ats-components/water-indicator';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -8,9 +9,11 @@ const instructions = Platform.select({
 
 export default class App extends Component {
   render() {
+    let steps = [1,2,3,4];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <WaterIndicator steps={steps}/>
+        <Text style={styles.welcome}>Welcome to React Irrigate ono!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
